49d96b9de7e093e2aebe682fe162a637709b503b
Kavya O S <kavya.os15@gmail.com>
2021-05-31 21:33:02 -0700

    binary search recurssive added

--- binary_search.py
+++ binary_search.py
@@ -1,1 +1,16 @@
-def 
+def binary_search(arr, low, high, key):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] > key:
+            return binary_search(arr, low, mid-1, key)
+        else:
+            return binary_search(arr, mid+1, high, key)
+    else:
+        return -1
+
+arr = [1, 2, 3, 4, 5, 10]
+key = 10
+j = binary_search(arr, 0, len(arr)-1, key)
+print("Element found at index:", str(j))
